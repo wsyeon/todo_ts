eb19e48d204fcbf61f6a889a4a299def56954877
C팀 <sywon2005@gmail.com>
2023-04-19 17:11:54 +0900

    클릭 시 삭제 및 useRef로 아이값 증가

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,32 +1,59 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useRef, useState } from 'react';
 import TodoList from './TodoList';
 // import * as S from '../style/TodoStyle';
 
 export interface Todo  {
     id: number,
     text: string,
-    checked: boolean
+    checked: boolean,
+    date: Date
 };
 
 const TodoInput: React.FC = ()=> {
-    const [number, setNumber] = useState<number>(1);
     const [inputText, setInputText] = useState("");
-    const [todoList, setTodoList] = useState<Todo[]>([])
+    const [todoList, setTodoList] = useState<Todo[]>([]);
+    const date = new Date();
+    const nextId = useRef(1);
 
     const onChange = (e: ChangeEvent<HTMLInputElement>)=> {
         setInputText(e.target.value);
     };
 
     const onClick = ()=> {
-        setNumber(number + 1);
         const newTodoList = {
-            id: number,
+            id: nextId.current,
             text: inputText,
-            checked: false
+            checked: false,
+            date: date
         };
 
-        setTodoList([...todoList, newTodoList]);
+        setTodoList(todoList.concat(newTodoList));
         setInputText("");
+        nextId.current += 1;
+    };
+
+    const handleDelete = (id: number)=> {
+        const updatedTodoList = todoList.filter((item) => item.id !== id);
+        setTodoList(updatedTodoList);
+    }; 
+
+    const checkBox = (id: number)=> {
+        setTodoList(todoList.map(data=> data.id === id ? {...data, checked: !data.checked} : data));
+    };
+
+    const keydownEnter = (e: React.KeyboardEvent<HTMLInputElement>)=> {
+        if (e.key === 'Enter' && e.nativeEvent.isComposing === false) {
+            const newTodoList = {
+                id: nextId.current,
+                text: inputText,
+                checked: false,
+                date: date
+            };
+    
+            setTodoList(todoList.concat(newTodoList));
+            setInputText("");
+            nextId.current += 1;
+        }
     };
 
     return (
@@ -36,12 +63,12 @@ const TodoInput: React.FC = ()=> {
                     오늘의 할 일
                 </div>
                 <div>
-                    <input type='text' onChange={onChange} value={inputText} placeholder='오늘 할 일' />
+                    <input type='text' onKeyDown={keydownEnter} onChange={onChange} value={inputText} placeholder='오늘 할 일' />
                     <button onClick={onClick}>+</button>
                 </div>
                 <div>
                     {todoList.map((data, idx)=> (
-                        <TodoList key={idx} todo={data} />
+                        <TodoList key={idx} todo={data} onDelete={handleDelete} onCheckBox={checkBox} />
                     ))}
                 </div>
             </div>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import { Todo } from './TodoInput';
 
-interface TodoListProps {
-    todo?: Todo
+interface TodoProps {
+    todo?: Todo,
+    onDelete?: (id: number)=> void,
+    onCheckBox?: (id: number)=> void
 };
 
-const TodoList: React.FC<TodoListProps> = ({ todo })=> {
+const TodoList: React.FC<TodoProps> = ({ todo, onDelete, onCheckBox })=> {
+
+    const upDate = ()=> {
+        if (todo && onDelete) {
+            onDelete(todo.id);
+        }
+    };
+
+    const  checkBoolean = ()=> {
+        if (todo && onCheckBox) {
+            onCheckBox(todo.id);
+        }
+    };
+
     return (
         <div>
-            <div>
-                {todo?.id}번 {todo?.text}
-                <input type='checkbox' defaultChecked={todo?.checked} readOnly />   
-            </div>
+            {todo === undefined ? (<></>) : (
+                <div style={{display: "flex", justifyContent: 'space-between', width: '80%', border: '1px solid red'}}>
+                    <div>
+                        <input type='checkbox' onClick={checkBoolean} defaultChecked={todo.checked} />{todo.date.toLocaleDateString()}
+                    </div>
+                    <div>
+                        {todo.text}
+                    </div>
+                    <div>
+                        <button onClick={upDate} style={{ backgroundColor: "transparent", border: 'none'}}>X</button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
